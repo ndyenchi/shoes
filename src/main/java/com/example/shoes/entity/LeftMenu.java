e13103051772b8c7079d1016fd8846080b1f6670
DESKTOP-1KENKLS\ACER <nguyendoyen215@gmail.com>
2022-12-25 22:44:24 +0700

    left menu

---
+++ src/main/java/com/example/shoes/entity/LeftMenu.java
@@ -0,0 +1,2 @@
+package com.example.shoes.entity;public class LeftMenu {
+}
---
+++ src/main/java/com/example/shoes/repository/LeftMenuRepository.java
@@ -0,0 +1,4 @@
+package com.example.shoes.response;
+
+public interface LeftMenuRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.shoes.entity.LeftMenu, int> {
+}
